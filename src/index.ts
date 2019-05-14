1542cfd5b2dc30936d8a7ff261d4741bba8e6d59
Devon T. Wesley <dwesley89@gmail.com>
2019-05-14 13:30:11 -0700

    fix: added nodegit
    returning org repos list

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import { Application } from "probot" // eslint-disable-line no-unused-vars
+// const nodegit = require("nodegit") // eslint-disable-line no-unused-vars
+
+const ORG = "Oakland-Blockchain-Developers"
+const HEAD_REPO = "testing_repo"
+const HEAD_REMOTE = `https://github.com/${ORG}/${HEAD_REPO}.git`
+
+console.log("HEAD_REMOTE :: ==>", HEAD_REMOTE)
 
 export = (app: Application) => {
   app.on("push", async (context) => {
-    const { github, payload } = context
+    const { 
+      github: { repos },
+      payload: { repository, ref }
+    } = context
 
-    console.log('GITHUB :: ==>', github);
-    console.log('payload :: ==>', payload);
+    const isHeadPepo = (
+      repository.name === HEAD_REPO && ref === "refs/heads/master"
+    )
+
+    if (isHeadPepo) {
+      const { data } = await repos.listForOrg(
+        { 
+          org: ORG
+        }
+      )
+      
+      console.log("LIST :: ==>", data)
+    }
   })
 }
