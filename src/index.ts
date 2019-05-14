3d25ee0f203ab201d2a48af85db8d206b9fc1acf
Devon T. Wesley <dwesley89@gmail.com>
2019-05-14 01:12:57 -0700

    feat: created scripts for build

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from "probot" // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on("push", async (context) => {
+    const { github, payload } = context
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    console.log('GITHUB :: ==>', github);
+    console.log('payload :: ==>', payload);
+  })
 }
