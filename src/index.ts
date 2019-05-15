37dc6c45ae8c80104b674af8186c8d8057b2f7d6
Devon T. Wesley <dwesley89@gmail.com>
2019-05-15 14:20:28 -0700

    WIP: throw away commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { Application } from "probot" // eslint-disable-line no-unused-vars
-const { Clone, Remote } = require("nodegit") // eslint-disable-line no-unused-vars
+const { Clone /**, Branch, Cherrypick*/} = require("nodegit") // eslint-disable-line no-unused-vars
 
 const ORG = "Oakland-Blockchain-Developers" // TODO: change to OPEN_RPC org.
 const HEAD_REPO = "testing_repo" // TODO: change to Pristine
-const HEAD_REMOTE_URL = `https://github.com/${ORG}/${HEAD_REPO}.git`
-const HEAD_REMOTE_ORIGIN = "test"
+// const HEAD_PULL_BRANCH = 'feat/new_pristine_changes'
 
 export = (app: Application) => {
   app.on("push", async (context) => {
     const { 
       github: { repos },
-      payload: { repository, ref }
+      payload: { repository, ref, after }
     } = context
 
     const isHeadPepo = (
@@ -18,26 +17,53 @@ export = (app: Application) => {
     )
 
     if (isHeadPepo) {
-      const { data } = await genericAsyncFunction(repos.listForOrg,[
+      const { data } = await genericAsyncFunction(repos.listForOrg, [
         { 
           org: ORG 
         }
       ])
-      
-      data.map((repo: any) => {
-        return Clone(repo.clone_url, `tmp/repos/${repo.name}`)
-          .then((clone: any) => {
-            Remote.setUrl(
-              clone,
-              HEAD_REMOTE_ORIGIN,
-              HEAD_REMOTE_URL
-            )
 
-            const fetchResult = Promise.resolve(clone.fetchAll())
-
-            return { clone, fetchResult }
+      Clone.clone(repository.clone_url, `tmp/head/${HEAD_REPO}`)
+        .then((repo: any) => {
+          return repo.getMasterCommit();
+        })
+        .then((firstCommitOnMaster: any) => {
+            return firstCommitOnMaster.getTree();
+        })
+        .then((tree: any) => {
+          data.forEach((repo: any) => {
+            Clone.clone(repo.clone_url, `tmp/repos/${repo.name}`)
+              .then((clone: any) => {
+                clone.getMasterCommit()
+                  .then((commit: any) => {
+                    commit.getTree()
+                      .then((subtree: any) => {
+                        console.log("SUB_TREE :: ==>", subtree)
+                        return subtree.diff(tree, () => {})
+                      })
+                      .then((diffList: any) => {
+                        console.log("DIFF_LIST :: ==>", diffList)
+                      })
+                      .catch((error: any) => { console.log("ERROR_TREE :: ==>", error) })
+                    // Branch.create(clone, HEAD_PULL_BRANCH, commit, 1)
+                    //   .then(() => {
+                    //     return commit.getTree()
+                    //   })
+                    //   .then((subTree: any) => { 
+                    //     return subTree.diff(tree, () => { console.log("DIFF_CALLBACK") })
+                    //   })
+                    //   .then((diffList: any) => {
+                    //     console.log("difflist :: ==>", diffList)
+                    //   })
+                    //   .catch((error: any) => {
+                    //     console.error("ERROR :: =>", error)
+                    //   })
+    
+                    return commit
+                  })
+              })
           })
-      })
+        })
     }
   })
 }
