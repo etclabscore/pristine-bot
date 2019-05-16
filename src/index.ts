1e616ff48a30610ff0192f783f89a7e12c025c58
Devon T. Wesley <dwesley89@gmail.com>
2019-05-15 19:15:55 -0700

    feat: refactored app
    different strategy, no nodegit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { Application } from "probot" // eslint-disable-line no-unused-vars
-const { Clone /**, Branch, Cherrypick*/} = require("nodegit") // eslint-disable-line no-unused-vars
 
 const ORG = "Oakland-Blockchain-Developers" // TODO: change to OPEN_RPC org.
 const HEAD_REPO = "testing_repo" // TODO: change to Pristine
-// const HEAD_PULL_BRANCH = 'feat/new_pristine_changes'
+const HEAD_PULL_BRANCH = 'feat/new_pristine_changes'
 
 export = (app: Application) => {
   app.on("push", async (context) => {
     const { 
-      github: { repos },
-      payload: { repository, ref, after }
+      github: { repos, gitdata, pullRequests},
+      payload: { repository, ref, head_commit }
     } = context
 
     const isHeadPepo = (
@@ -21,58 +20,111 @@ export = (app: Application) => {
         { 
           org: ORG 
         }
-      ])
+      ] /**, console.log*/)
 
-      Clone.clone(repository.clone_url, `tmp/head/${HEAD_REPO}`)
-        .then((repo: any) => {
-          return repo.getMasterCommit();
-        })
-        .then((firstCommitOnMaster: any) => {
-            return firstCommitOnMaster.getTree();
-        })
-        .then((tree: any) => {
-          data.forEach((repo: any) => {
-            Clone.clone(repo.clone_url, `tmp/repos/${repo.name}`)
-              .then((clone: any) => {
-                clone.getMasterCommit()
-                  .then((commit: any) => {
-                    commit.getTree()
-                      .then((subtree: any) => {
-                        console.log("SUB_TREE :: ==>", subtree)
-                        return subtree.diff(tree, () => {})
-                      })
-                      .then((diffList: any) => {
-                        console.log("DIFF_LIST :: ==>", diffList)
-                      })
-                      .catch((error: any) => { console.log("ERROR_TREE :: ==>", error) })
-                    // Branch.create(clone, HEAD_PULL_BRANCH, commit, 1)
-                    //   .then(() => {
-                    //     return commit.getTree()
-                    //   })
-                    //   .then((subTree: any) => { 
-                    //     return subTree.diff(tree, () => { console.log("DIFF_CALLBACK") })
-                    //   })
-                    //   .then((diffList: any) => {
-                    //     console.log("difflist :: ==>", diffList)
-                    //   })
-                    //   .catch((error: any) => {
-                    //     console.error("ERROR :: =>", error)
-                    //   })
-    
-                    return commit
-                  })
+      data.forEach(async (repo: any) => {
+        const { data: { sha } } = await genericAsyncFunction(repos.getCommitRefSha, [{
+          owner: ORG,
+          repo: repo.name,
+          ref: "refs/heads/master"
+        }] /**, console.log*/)
+
+        const refExists = await genericAsyncFunction(repos.getCommitRefSha, [{
+          owner: ORG,
+          repo: repo.name,
+          ref: `heads/${HEAD_PULL_BRANCH}`
+        }] /**, console.log*/)
+
+        console.log('REF_EXISTS :: =>', refExists)
+
+        if(!refExists) {
+          await genericAsyncFunction(gitdata.createRef, [{
+            owner: ORG,
+            repo: repo.name,
+            ref: `refs/heads/${HEAD_PULL_BRANCH}`,
+            sha
+          }] /**, console.log*/)
+        }
+
+        // this gets you file sha
+        const contents = head_commit.modified.map(async (path: string) => {
+          const oldContent = await genericAsyncFunction(repos.getContents, [{
+            owner: ORG,
+            repo: repo.name,
+            path
+          }] /**, console.log*/)
+
+          console.log("OLD_CONTENTS :: ==>", oldContent)
+
+          const { data: { content } } = await genericAsyncFunction(repos.getContents, [{
+            owner: ORG,
+            repo: HEAD_REPO,
+            path
+          }] /**, console.log*/)
+
+          if (oldContent) {  
+            oldContent.data.updatedContents = content
+            oldContent.fileExists = true
+            return oldContent
+          } else {
+            return new Promise((resolve) => {
+              resolve({ 
+                fileExists: false, 
+                data: { updatedContents: content, name: path } 
               })
-          })
+            })
+          }
         })
+
+        const result = await Promise.all(contents)
+
+        result.forEach((file: any) => {
+          console.log("FILE :: ==>", file)
+
+          if(file.fileExists) {
+            console.log("UPDATE !!!");
+            console.log("FILE :: ++>", file)
+            genericAsyncFunction(repos.updateFile, [{
+              owner: ORG,
+              repo: repo.name,
+              path: file.data.name,
+              message: "feat: new pristine changes",
+              content: file.data.updatedContents,
+              sha: file.data.sha,
+              branch: `refs/heads/${HEAD_PULL_BRANCH}`
+            }] /**, console.log*/)
+          } else {
+            console.log("CREATE !!!");
+            genericAsyncFunction(repos.createFile, [{
+              owner: ORG,
+              repo: repo.name,
+              path: file.data.name,
+              message: "feat: new pristine changes",
+              content: file.data.updatedContents,
+              branch: `refs/heads/${HEAD_PULL_BRANCH}`
+            }] /**, console.log*/)
+          }
+        })
+
+        await genericAsyncFunction(pullRequests.create, [{
+          owner: ORG,
+          repo: repo.name,
+          title: "Pristine has been updated!",
+          head: `refs/heads/${HEAD_PULL_BRANCH}`,
+          base: "refs/heads/master"
+        }] /**, console.log*/)
+      })
     }
   })
 }
 
-async function genericAsyncFunction(func: any, args: any) {
+async function genericAsyncFunction(func: any, args: any /**, logger: any*/) {
+  // if(logger) logger(`Method: ${func.name}\n${JSON.stringify(args)}`)
+
   try {
     const result = await func(...args)
     return result
   } catch (error) {
-    console.error(`ERROR: ${error.message}`)
+    console.error(`ERROR: ${JSON.stringify(error.message, null, 4)}`)
   }
 }
