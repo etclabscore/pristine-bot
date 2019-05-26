bccabe00dc997787e57d54d4cd4bb69b3c0c2018
Devon T. Wesley <dwesley89@gmail.com>
2019-05-26 13:24:06 -0700

    feat: one commit per repo.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Application } from "probot" // eslint-disable-line no-unused-vars
-import MasterRepo from "./publish_repo"
 
-const HEAD_REPO = "Pristine" // Abstract
+const HEAD_REPO = "Pristine"
+const ORG = "Oakland-Blockchain-Developers"
+const BRANCH_REF = "refs/heads/feat/pristine"
 
 export = (app: Application) => {
   app.on("push", async (context) => {
     const { 
       github: { repos, gitdata, pullRequests },
-      payload: { repository, ref, head_commit, after }
+      payload: { repository, ref, head_commit }
     } = context
 
     const isPublishRepo = (
@@ -15,108 +16,96 @@ export = (app: Application) => {
     )
 
     if (isPublishRepo)  {
-      const masterRepo = new MasterRepo({
-        owner: "Oakland-Blockchain-Developers",
-        repo: "Pristine",
-        gitAPI: context.github,
-        payload: context.payload,
-        headPullBranch: 'feat/pristine_changes'
-      })
-
-      const ORG = masterRepo.getOwner()
-      const MASTER_REF = masterRepo.createNewRefText(
-        masterRepo.getHeadPullBranch(), 
-        after.substring(0, 7)
-      )
-
-      const { data } = await genericAsyncFunction(repos.listForOrg, [
+      const reposList = await genericAsyncFunction(repos.listForOrg, [
         { 
           org: ORG
         }
       ] /**, console.log*/)
 
-      data.forEach(async (repo: any) => {
-        const { data: { sha } } = await genericAsyncFunction(repos.getCommitRefSha, [{
+      const repoNames = reposList.data.map(async (repo: any) => {
+        const oldCommit = await genericAsyncFunction(repos.getCommitRefSha, [{
           owner: ORG,
           repo: repo.name,
           ref: "refs/heads/master"
         }] /**, console.log*/)
 
+        console.log("OLD_DATA :: ==>", oldCommit.data.sha)
+
         await genericAsyncFunction(gitdata.createRef, [{
           owner: ORG,
           repo: repo.name,
-          ref: MASTER_REF,
-          sha
+          ref: BRANCH_REF,
+          sha: oldCommit.data.sha
         }] /**, console.log*/)
 
-        // this gets you file sha
-        const contents = head_commit.modified.map(async (path: string) => {
-          
-          // TODO: moving to publish_repo.ts
-          const oldContent = await genericAsyncFunction(repos.getContents, [{
-            owner: ORG,
-            repo: repo.name,
-            path
-          }] /**, console.log*/)
-
+        const filesToCommit: any = head_commit.modified.map(async (path: string) => {
           const { data: { content } } = await genericAsyncFunction(repos.getContents, [{
             owner: ORG,
             repo: HEAD_REPO,
-            path
+            path,
+            ref: "refs/heads/master"
           }] /**, console.log*/)
 
-          if (oldContent) {  
-            oldContent.data.updatedContents = content
-            oldContent.fileExists = true
-            return oldContent
-          } else {
-            return new Promise((resolve) => {
-              resolve({ 
-                fileExists: false, 
-                data: { updatedContents: content, name: path } 
-              })
-            })
+          const buff = Buffer.from(content, 'base64');  
+          const text = buff.toString();
+
+          const blob = await genericAsyncFunction(gitdata.createBlob, [{ 
+            owner: ORG,
+            repo: repo.name,
+            content: text
+          }])
+
+          return {
+            sha: blob.data.sha,
+            path,
+            mode: '100644',
+            type: 'blob'
           }
         })
 
-        const result = await Promise.all(contents)
+        const fileBlobs = await Promise.all(filesToCommit)
 
-        result.forEach((file: any) => {
-          console.log("FILE :: ==>", file)
+        const newTree = await genericAsyncFunction(gitdata.createTree, [{ 
+          owner: ORG,
+          repo:  repo.name,
+          tree: fileBlobs,
+          base_tree: oldCommit.data.commit.tree.sha
+        }])
 
-          if(file.fileExists) {
-            console.log("UPDATE !!!");
-            console.log("FILE :: ++>", file)
-            genericAsyncFunction(repos.updateFile, [{
-              owner: ORG,
-              repo: repo.name,
-              path: file.data.name,
-              message: "feat: new pristine changes",
-              content: file.data.updatedContents,
-              sha: file.data.sha,
-              branch: MASTER_REF
-            }] /**, console.log*/)
-          } else {
-            console.log("CREATE !!!");
-            genericAsyncFunction(repos.createFile, [{
-              owner: ORG,
-              repo: repo.name,
-              path: file.data.name,
-              message: "feat: new pristine changes",
-              content: file.data.updatedContents,
-              branch: MASTER_REF
-            }] /**, console.log*/)
-          }
-        })
+        console.log("NEW_TREE :: ==>", newTree.data.tree)
 
-        await genericAsyncFunction(pullRequests.create, [{
+        const newCommit = await genericAsyncFunction(gitdata.createCommit, [{
           owner: ORG,
           repo: repo.name,
+          parents: [oldCommit.data.sha],
+          tree: newTree.data.sha,
+          message: "feat: new pristine changes"
+        }])
+
+        console.log("NEW_COMMIT :: ==>", newCommit.data.sha)
+
+        await genericAsyncFunction(gitdata.updateRef, [{
+          owner: ORG,
+          repo: repo.name,
+          ref: "heads/feat/pristine",
+          sha: newCommit.data.sha,
+          force: true
+        }] /**, console.log*/)
+
+        return repo.name;
+      })
+
+      const names = await Promise.all(repoNames)
+
+      names.forEach((name: any) => {
+        genericAsyncFunction(pullRequests.create, [{
+          owner: ORG,
+          repo: name,
           title: "Pristine has been updated!",
-          head: MASTER_REF,
+          head: BRANCH_REF,
           base: "refs/heads/master"
         }] /**, console.log*/)
-      })
+      });
     }
   })
 }
@@ -128,6 +117,6 @@ async function genericAsyncFunction(func: any, args: any /**, logger: any*/) {
     const result = await func(...args)
     return result
   } catch (error) {
-    console.error(`ERROR: ${JSON.stringify(error.message, null, 4)}`)
+    console.error("THESE_ARE_ERRORS :: ==>", error)
   }
 }
